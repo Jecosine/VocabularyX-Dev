d12e6197f6c0af65cf2aaa87c552736254009790
JecosineMoc <mj_ol@outlook.com>
2021-02-10 10:47:44 +0800

    UPDATE: Finish titlebar

--- MainApp/src/renderer/components/partial/Titlebar.vue
+++ MainApp/src/renderer/components/partial/Titlebar.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2021-02-06 21:16:20
  * @LastEditors: Jecosine
- * @LastEditTime: 2021-02-10 09:38:53
+ * @LastEditTime: 2021-02-10 10:26:58
 -->
 <template>
   <div id="title-container" :style="{'height': height, 'lineHeight': height}">
@@ -9,12 +9,21 @@
       <font-awesome-icon :icon="['fab', 'artstation']" class="title-icon"></font-awesome-icon>
       <div class="title-self">{{ title }}</div>      
     </div>
-    <div class="title-button-group">
-      <div id="button-minimize">
-        <font-awesome-icon :icon="['fas','user']"></font-awesome-icon>
+    <div class="title-button-group" :style="{'height': height}">
+      <div id="button-minimize" class="window-button" :style="{'width': height, 'height': height}">
+        <!-- <font-awesome-icon :icon="['far','window-minimize']"></font-awesome-icon> -->
+        <i class="el-icon-minus"></i>
+
       </div>
-      <div id="button-maximum"></div>
-      <div id="button-close"></div>
+      <div id="button-maximum" class="window-button" :style="{'width': height, 'height': height}">
+        <!-- <font-awesome-icon :icon="['far','square']"></font-awesome-icon> -->
+        <i class="el-icon-upload2"></i>
+
+      </div>
+      <div id="button-close" class="window-button" :style="{'width': height, 'height': height}">
+        <!-- <font-awesome-icon :icon="['fas','times']"></font-awesome-icon> -->
+        <i class="el-icon-close"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -46,10 +55,34 @@ export default {
   font-size: 14px;
   color: var(--color-text-primary);
 }
+.title-button-group {
+  position: absolute;
+  display: flex;
+  top: 0;
+  right: -1.125rem;
+}
 .title-icon,
 .title-self {
   display: inline-block;
   line-height: 100%;
   color: var(--color-primary);
+}
+.title-self {
+  margin-left: 1rem;
+}
+.window-button {
+  display: inline-block;
+  position: relative;
+  height: 100%;
+  text-align: center;
+  /* line-height: 100%; */
+  color: var(--color-primary);
+}
+.window-button:hover {
+  background-color: var(--color-primary-secondary);
+}
+#button-close {
+  font-weight: 800;
+  font-size: 18px;
 }
 </style>
