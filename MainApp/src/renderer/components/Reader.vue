5ade99f4b84973c7250c1954d6983aa03cb98a05
JecosineMoc <mj_ol@outlook.com>
2021-02-20 18:36:10 +0800

    ADD: Add Reader vue and modify build configuration

---
+++ MainApp/src/renderer/components/Reader.vue
@@ -0,0 +1,51 @@
+<!--
+ * @Date: 2021-02-20 18:27:52
+ * @LastEditors: Jecosine
+ * @LastEditTime: 2021-02-20 18:36:09
+-->
+<template>
+  <el-container id="reader-container">
+    <el-main id="reader-main">
+      <Word word="currentWord"/>
+    </el-main>
+    <el-aside id="reader-aside">
+      
+    </el-aside>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentWord: ''
+    }
+  },
+  methods: {},
+  mounted () {
+    // todo render aside catalog
+    // ...
+    // test
+  },
+  created () {
+    // todo get last record
+    // ...
+  }
+}
+</script>
+
+<style scoped>
+#reader-container {
+  width: 100%;
+  height: 100%;
+}
+#reader-main {
+  background-color: aqua;
+  padding: 0;
+  margin: 0;
+
+}
+#reader-aside {
+  background-color: beige;
+}
+</style>
